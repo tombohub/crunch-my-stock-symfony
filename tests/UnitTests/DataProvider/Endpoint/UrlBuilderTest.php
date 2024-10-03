30aa9861eb467623b15a98df78c3b666e35c5f91
me <tkutlesa@gmail.com>
2024-10-03 18:51:44 -0400

    url builder expanding path

--- tests/UnitTests/DataProvider/Endpoint/UrlBuilderTest.php
+++ tests/UnitTests/DataProvider/Endpoint/UrlBuilderTest.php
@@ -5,18 +5,44 @@ namespace App\Tests\UnitTests\DataProvider\Endpoint;
 use App\DataProvider\Endpoint\UrlBuilder;
 
 use PHPUnit\Framework\TestCase;
-use ReflectionClass;
 
 class UrlBuilderTest extends TestCase
 {
-    public function test()
+    private UrlBuilder $urlBuilder;
+    protected function setUp(): void
     {
-        $urlBuilder = new UrlBuilder;
+        $this->urlBuilder = new UrlBuilder;
+    }
+    public function test_build_onePathVariable()
+    {
         $urlTemplate = 'https://www.url.com/{stock}';
         $params = ['stock' => 'AAPL'];
-        $actualUrl = $urlBuilder->build($urlTemplate, $params);
+        $actualUrl = $this->urlBuilder->build($urlTemplate, $params);
 
         $expectedUrl = 'https://www.url.com/AAPL';
+
+        $this->assertEquals($expectedUrl, $actualUrl);
+    }
+
+    public function test_build_twoPathVariables()
+    {
+        $urlTemplate = 'https://www.url.com/{stock}/{date}';
+        $params = ['stock' => 'AAPL', 'date' => '2022-03-03'];
+        $actualUrl = $this->urlBuilder->build($urlTemplate, $params);
+
+        $expectedUrl = 'https://www.url.com/AAPL/2022-03-03';
+
+        $this->assertEquals($expectedUrl, $actualUrl);
+    }
+
+    public function test_build_oneQueryParam()
+    {
+        $urlTemplate =
+            'https://www.url.com/';
+        $params = ['stock' => 'AAPL'];
+        $actualUrl = $this->urlBuilder->build($urlTemplate, $params);
+
+        $expectedUrl = 'https://www.url.com/?stock=AAPL';
 
         $this->assertEquals($expectedUrl, $actualUrl);
     }
